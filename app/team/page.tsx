201225247aa7ad2869e7de0a10b96b12640f9868
Suryash Kumar Jha | IT | Pramericalife <P037343@pramericalife.in>
2025-03-19 00:06:28 +0530

    Team Page started'

---
+++ app/team/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Layout} from '../../components/layout'
+
+const Team = () => {
+    return (
+        <>
+            <Layout >
+                <div>Team</div>
+            </Layout>
+        </>
+    )
+}
+
+export default Team
