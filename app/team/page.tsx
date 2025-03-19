8e860c2874320689346ddcfc19346aa3196cf7f2
Suryash Kumar Jha | IT | Pramericalife <P037343@pramericalife.in>
2025-03-19 22:24:07 +0530

    team page

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -1,11 +1,26 @@
+'use client'
 import React from 'react'
-import {Layout} from '../../components/layout'
+import { Layout } from '../../components/layout'
+import { ProjectBoard } from '@/components/project-board'
+import { useSelector } from 'react-redux'
+import { RootState } from '@/redux/store'
+import { Select, Option } from '@mui/joy'
 
 const Team = () => {
+    const { taskList, loading } = useSelector((state: RootState) => state.task)
+
     return (
         <>
             <Layout >
-                <div>Team</div>
+
+                <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">
+                    {/* <div>Team</div> */}
+                    <ProjectBoard taskList={taskList} viewOnly={true} />
+                    <Select>
+                        <Option value="dog">Dog</Option>
+                        <Option value="cat">Cat</Option>
+                    </Select>
+                </div>
             </Layout>
         </>
     )
